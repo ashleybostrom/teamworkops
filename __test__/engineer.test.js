721dda81f0822d5a638a1dd591fb864f564bd093
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-18 01:46:57 -0700

    added test for engineer

---
+++ __test__/engineer.test.js
@@ -0,0 +1,21 @@
+const Engineer = require('../lib/engineer');
+const Employee = require('../lib/employee');
+const { test } = require('@jest/globals');
+
+test('creates an engineer object', () => {
+    const engineer = new Engineer('engineer1');
+
+    expect(typeof(engineer)).toBe('object');
+});
+
+test('get github username', () => {
+    const engineer = new Engineer('manager', '1', 'rosemary@thymeflies.com', 'rosemarygithub');
+
+    expect(engineer.github).toBe('rosemarygithub');
+});
+
+test('getRole() returns engineer', () => {
+    const engineer = new Engineer('manager1');
+
+    expect(engineer.getRole()).toBe('engineer');
+});
